2a2760bc73ee5d31b0b3b287497cd273db5aaf39
ahmettoklucu <103578139+ahmettoklucu@users.noreply.github.com>
2022-09-25 12:27:53 +0300

    Add files via upload

---
+++ WebApplication1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("ProductManagementDb") { }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
