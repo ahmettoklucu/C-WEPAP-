4f9cf2e8826bdf3ceda90cbc37d2b4ddf89d812f
ahmettoklucu <103578139+ahmettoklucu@users.noreply.github.com>
2022-09-25 12:42:35 +0300

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            //Product product = new Product()
+            //{
+            //    Name = "Telefon",
+            //    Description="Akıllı Telefon",
+            //    Price=15000,
+            //    Status=true,
+            //    IsDelete=false
+            //};
+            //db.Product.Add(product);
+            //db.SaveChanges();
+            return View();
+        }
+    }
+}
